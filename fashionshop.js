63dffb84cda152fefda9e1255903cf95936bf6bf
skday <haridwiharyono@gmail.com>
2016-11-23 15:26:38 +0700

    fixing view

--- fashionshop.js
+++ fashionshop.js
@@ -1,12 +1,40 @@
 (function () {
   'use strict';
+
   angular.module('fashionshop',['ui.router', 'ngMessages', 'ngStorage', 'ngRoute'])
     .config(config)
     .run(run);
 
   function config($stateProvider){
+
     $stateProvider
-      .state('')
+      .state('home', {
+        url:'/',
+				templateUrl:'home.html',
+        controller:'homeController',
+        controllerAs:'vm'
+      })
+      .state('login', {
+        url:'/login',
+        templateUrl:'login.html',
+        controller:'loginController',
+        controllerAs:'vm',
+        data:{
+          state:'login'
+        }
+      })
+      .state('registrasi',{
+        url:'/registrasi',
+        templateurl:'register.html',
+        controller:'registerController',
+        controllerAs:'vm',
+        data:{
+          state:'register'
+        }
+      });
+  };
+
+  function run($rootScope, $http, $location, $localStorage, authenticationService, $state) {
+
   }
 })();
-'ui_router'
