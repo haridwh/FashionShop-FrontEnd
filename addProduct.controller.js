bb3615bcfe285a210720218854a547685f640379
Matias Alvin <alvinmatias69@gmail.com>
2016-12-08 01:00:18 +0700

    list product image & image upload added

--- addProduct.controller.js
+++ addProduct.controller.js
@@ -2,9 +2,9 @@
   'use strict';
 
   angular.module('fashionshop')
-    .controller('addProductController', controller,  'ngFileUpload');
+    .controller('addProductController', controller);
 
-  function controller($http, $state, produkService){
+  function controller($rootScope, $http, $state, produkService, Upload){
     var vm=this;
 
     vm.nama='';
@@ -13,7 +13,9 @@
     vm.stok='';
     vm.harga='';
     vm.addProduk = addProduk;
+    vm.picFile = '';
     vm.isInserted=true;
+    vm.addProdukWithImage = addProdukWithImage;
 
     function addProduk(){
       produkService.addProduk(vm.nama, vm.deskripsi, vm.kategori, vm.stok, vm.harga, function (result) {
@@ -23,6 +25,25 @@
           vm.isInserted=false;
         }
       })
+    }
+
+    function addProdukWithImage(file) {
+      console.log('hmm');
+      file.upload = Upload.upload({
+        url: $rootScope.baseUrl + '/api/produk',
+        data:{
+          nama: vm.nama,
+          deskripsi: vm.deskripsi,
+          kategori: vm.kategori,
+          stok: vm.stok,
+          harga: vm.harga,
+          image: file
+        },
+      });
+
+      file.upload.then(function(response) {
+        $state.go('listProduct');
+      });
     }
   }
 })();
